bc64e8b4e918a635699ce47f99f2a40b2299a954
gsv2111 Gabriel Ventura <gsv2111@columbia.edu>
2024-06-29 00:10:18 +0000

    bug fix

--- test_calc.py
+++ test_calc.py
@@ -16,11 +16,11 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(self.calc.mul(2, 3), 6)
 
     def test_div(self):
-	self.assertEqual(self.calc.div(6, 2), 3)
+        self.assertEqual(self.calc.div(6, 2), 3)
 
     def test_pow(self):
-	self.assertEqual(self.calc.pow(6, 2), 36)
+        self.assertEqual(self.calc.pow(6, 2), 36)
 
     def test_sqrt(self):
-	self.assertEqual(self.calc.sqrt(4), 2)
+        self.assertEqual(self.calc.sqrt(4), 2)
 
